8517dd1a63e498caaa8e51c3360baf5a85e4dc0e
jamiegitup <gitlearningjourney@gmail.com>
2024-07-10 16:05:29 -0500

    Update Cafe-Delights-Coffee.sql

--- Cafe-Delights-Coffee.sql
+++ Cafe-Delights-Coffee.sql
@@ -1,1 +1,12 @@
-SELECT * FROM tmp
+SELECT
+   product_category,
+   Month,
+   SUM(Total_Bill) AS total_sales
+FROM
+   coffee
+GROUP BY
+   product_category,
+   Month
+ORDER BY
+   Month,
+   total_sales DESC;
