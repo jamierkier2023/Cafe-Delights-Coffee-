bc3cb676465d06279e4f7b24c61280804deaf305
jamiegitup <157421973+jamierkier2023@users.noreply.github.com>
2024-07-10 16:32:58 -0500

    Update Cafe-Delights-Coffee.sql

--- Cafe-Delights-Coffee.sql
+++ Cafe-Delights-Coffee.sql
@@ -1,3 +1,31 @@
+
+SELECT * FROM tmp
+CREATE TABLE coffee(
+  transaction_id INT PRIMARY key,
+  transaction_date TEXT,
+  transaction_time TEXT,
+  store_id INT,
+  store_location TEXT,
+  product_id INT,
+  transaction_qty INT,
+  unit_price REAL,
+  Total_bill REAL,
+  product_category TEXT
+  product_type TEXT,
+  product_detail TEXT
+  size TEXT,
+  month_name TEXT,
+  Day_name TEXT,
+  hour INT,
+  month INT,
+  day_of_week INT,
+);
+INSERT INTO coffee
+transaction_id, transaction_date, transaction_time, store_id, store_location, product_id, transaction_qty, unit_price, Total_bill, product_category, product_type, product_detail, size, month_name, Day_name, hour, month, day_of_week
+)
+SELECT transaction_id, transaction_date, transaction_time, store_id, store_location, product_id, transaction_qty, unit_price, Total_bill, product_category, product_type, product_detail, size, month_name, Day_name, hour, month, day_of_week
+FROM tmp;
+SELECT * FROM coffee;
 SELECT
    product_category,
    Month,
